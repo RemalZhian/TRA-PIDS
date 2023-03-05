c8cba5d0ef8e6a6a77c9bff9b2315e4b583bf957
RemalZhian <126994410+RemalZhian@users.noreply.github.com>
2023-03-05 09:05:07 +0800

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,9 +1,9 @@
 var diagram = {
 
-	"title_ch": "中壢火車站",
+	"title_ch": "板橋火車站",
 	//中文站名 Main Title
 
-	"title_en": "Zhongli Railway Station",
+	"title_en": "Banqiao Railway Station",
 	//英文站名 English Title
 
 	"platform": "1",
@@ -15,52 +15,52 @@ var diagram = {
 	"platform_en": "PLAT.",
 	//月台英文標題 English Plarfotm Title
 
-	"time": "09:38",
+	"time": "09:00",
 	//時間 Time (留白不顯示)
 
 	"data":
 	[
 		{
-			"train_no": "1180",
+			"train_no": "1138",
 			"destination": "基隆",
 			"train_type": "區間車",
-			"dept_time": "09:49",
+			"dept_time": "09:02",
 			"via": "山線",
 			"remarks": "準點"
 		},
 		{
-			"train_no": "502",
-			"destination": "七堵",
-			"train_type": "莒光",
-			"dept_time": "10:04",
-			"via": "山線",
+			"train_no": "278",
+			"destination": "花蓮",
+			"train_type": "普悠瑪",
+			"dept_time": "09:08",
+			"via": "海線",
 			"remarks": "準點"
 
 		},
 		{
-			"train_no": "1186",
-			"destination": "基隆",
+			"train_no": "4162",
+			"destination": "蘇澳",
 			"train_type": "區間車",
-			"dept_time": "10:15",
-			"via": "",
+			"dept_time": "09:13",
+			"via": "宜蘭",
 			"remarks": "準點"
 
 		},
 		{
-			"train_no": "108",
-			"destination": "七堵",
+			"train_no": "418",
+			"destination": "臺東",
 			"train_type": "自強",
-			"dept_time": "10:28",
-			"via": "山線",
+			"dept_time": "09:19",
+			"via": "東線",
 			"remarks": "準點"
 
 		},
 		{
-			"train_no": "1188",
+			"train_no": "2120",
 			"destination": "基隆",
 			"train_type": "區間車",
-			"dept_time": "10:39",
-			"via": "",
+			"dept_time": "09:23",
+			"via": "山線",
 			"remarks": "準點"
 		}
 	]
